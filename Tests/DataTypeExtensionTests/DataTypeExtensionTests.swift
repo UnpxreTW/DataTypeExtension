66db2d533f1e95d33a89cbe31dfe7c24190727d8
Ultracker <aron@ultracker.com>
2020-03-17 10:44:21 +0800

    Update DataTypeExtensionTests.swift

--- Tests/DataTypeExtensionTests/DataTypeExtensionTests.swift
+++ Tests/DataTypeExtensionTests/DataTypeExtensionTests.swift
@@ -6,7 +6,6 @@ final class DataTypeExtensionTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(DataTypeExtension().text, "Hello, World!")
     }
 
     static var allTests = [
