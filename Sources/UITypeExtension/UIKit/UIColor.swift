c160a6790e725721002b4779592fee6d45723ca7
Unpxre <unpxre.tw@gmail.com>
2020-07-02 10:43:12 +0800

    [add] 從整數初始化 `UIColor`

--- Sources/UITypeExtension/UIKit/UIColor.swift
+++ Sources/UITypeExtension/UIKit/UIColor.swift
@@ -12,4 +12,12 @@ extension UIColor {
     public convenience init(red: Int, green: Int, blue: Int) {
         self.init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: 1)
     }
+
+    public convenience init(hexadecimal: Int) {
+        self.init(
+            red: (hexadecimal >> 16) & 0xFF,
+            green: (hexadecimal >> 8) & 0xFF,
+            blue: (hexadecimal) & 0xFF
+        )
+    }
 }
